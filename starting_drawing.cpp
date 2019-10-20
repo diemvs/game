af85669112e557c9491030c2b7f1fc2d68dd8260
Diemvs <sataev.dima@mail.ru>
2019-10-20 12:05:48 +0300

    added level

---
+++ starting_drawing.cpp
@@ -0,0 +1,6 @@
+#include "starting_drawing.h"
+
+starting_drawing::starting_drawing()
+{
+
+}
---
+++ starting_drawing.h
@@ -0,0 +1,11 @@
+#ifndef STARTING_DRAWING_H
+#define STARTING_DRAWING_H
+
+
+class starting_drawing
+{
+public:
+    starting_drawing();
+};
+
+#endif // STARTING_DRAWING_H
